2f7302f7439991c4f0143498616cc46fc167c27b
Kenneth V. Langga <klangga@gmail.com>
2016-11-23 19:09:09 +0800

    Added checksum verification action

--- checksums.py
+++ checksums.py
@@ -7,7 +7,7 @@ import os
 import subprocess
 import sys
 
-_version = '0.3'
+_version = '0.4'
 print(os.path.basename(__file__) + ': v' + _version)
 _logger = logging.getLogger()
 _LOG_LEVEL = logging.DEBUG
@@ -24,7 +24,7 @@ for bin in BINS:
         exit(1)
 
 
-def _get_dir_checksums(dir_path):
+def _load_files(dir_path):
     # Check if dir path exists
     if not os.path.isdir(dir_path):
         _logger.error('%s does not exist! Exiting.')
@@ -32,7 +32,6 @@ def _get_dir_checksums(dir_path):
 
     _logger.info('Processing dir: %s', dir_path)
     checksums = {}
-    old_checksums = {}
     # Check if SHA1SUMS file already exists
     sha1sum_filepath = os.path.join(dir_path, 'SHA1SUMS')
     if os.path.isfile(sha1sum_filepath):
@@ -44,45 +43,61 @@ def _get_dir_checksums(dir_path):
                 fn = tokens[1]
                 if fn.startswith('?'):
                     fn = fn[1:]
-                old_checksums[fn] = tokens[0]
+                checksums[fn] = tokens[0]
 
     last_modified = {}
-    old_last_modified = {}
     # Check if LAST_MODIFIED file already exists
     last_modified_filepath = os.path.join(dir_path, 'LAST_MODIFIED')
     if os.path.isfile(last_modified_filepath):
-        old_last_modified = json.load(open(last_modified_filepath, 'r'))
+        last_modified = json.load(open(last_modified_filepath, 'r'))
 
+    return (checksums, sha1sum_filepath,
+            last_modified, last_modified_filepath)
+
+
+def _list_files(dir_path):
     # List dir contents
     for f in sorted(os.listdir(dir_path)):
         file_path = os.path.join(dir_path, f)
         if (f != 'SHA1SUMS' and f != 'LAST_MODIFIED' and not f.startswith('.')
                 and os.path.isfile(file_path)):
-            compute_checksum = False
+            yield f, file_path
 
-            # Compute checksum if file hasn't been computed yet
-            if not f in old_checksums:
-                _logger.info("File does not have checksum. Computing checksum: %s",
-                             file_path)
-                compute_checksum = True
 
-            # Get last modified time of file
-            lmt = os.stat(file_path).st_mtime
-            # Recompute checksum if file has been modified
-            if f in old_last_modified and lmt > old_last_modified[f]:
-                _logger.info("File has been modified. Computing checksum: %s",
-                             file_path)
-                compute_checksum = True
+def _generate(dir_path):
+    # Load files
+    (old_checksums, sha1sum_filepath,
+        old_last_modified, last_modified_filepath) = _load_files(dir_path)
+    checksums = {}
+    last_modified = {}
 
-            if compute_checksum:
-                # Compute checksum
-                shasum = subprocess.check_output(['shasum', file_path])
-                tokens = shasum.strip().split()
-                checksums[f] = tokens[0]
-            else:
-                checksums[f] = old_checksums[f]
+    for f, file_path in _list_files(dir_path):
 
-            last_modified[f] = lmt
+        compute_checksum = False
+
+        # Compute checksum if file hasn't been computed yet
+        if not f in old_checksums:
+            _logger.info("File does not have checksum. Computing checksum: %s",
+                         file_path)
+            compute_checksum = True
+
+        # Get last modified time
+        lmt = os.stat(file_path).st_mtime
+        # Recompute checksum if file has been modified
+        if f in old_last_modified and lmt > old_last_modified[f]:
+            _logger.info("File has been modified. Computing checksum: %s",
+                         file_path)
+            compute_checksum = True
+
+        if compute_checksum:
+            # Compute checksum
+            shasum = subprocess.check_output(['shasum', file_path])
+            tokens = shasum.strip().split()
+            checksums[f] = tokens[0]
+        else:
+            checksums[f] = old_checksums[f]
+
+        last_modified[f] = lmt
 
     # Write checksums to file
     if checksums:
@@ -95,12 +110,36 @@ def _get_dir_checksums(dir_path):
                   indent=4, sort_keys=True)
 
 
+def _verify(dir_path):
+    # Load files
+    checksums, _, last_modified, _ = _load_files(dir_path)
+
+    for f, file_path in _list_files(dir_path):
+
+        if f in checksums and f in last_modified:
+
+            # Compute checksum
+            shasum = subprocess.check_output(['shasum', file_path])
+            checksum = shasum.strip().split()[0]
+
+            # Get last modified time
+            lmt = os.stat(file_path).st_mtime
+
+            # Only compare checksums if file hasn't been modified
+            if lmt == last_modified[f]:
+                if checksum == checksums[f]:
+                    _logger.info('OK: %s', file_path)
+                else:
+                    _logger.warn('FAILED: %s', file_path)
+
+
 def parse_arguments():
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--version', action='version',
                         version=_version)
     parser.add_argument('-v', '--verbose', action="store_true")
+    parser.add_argument('action', choices=['generate', 'verify'])
     parser.add_argument('start_dir')
     args = parser.parse_args()
     return args
@@ -134,11 +173,11 @@ if __name__ == "__main__":
     # Parge arguments
     args = parse_arguments()
 
+    if args.action == 'verify':
+        _CONS_LOG_LEVEL = logging.WARN
+
     # Setup logging
     _setup_logging(args)
-
-    # Get checksum for dir
-    # _get_dir_checksums(os.path.abspath(args.dir))
 
     # Get checksums for all dirs in path
     start_path = os.path.abspath(args.start_dir)
@@ -147,4 +186,7 @@ if __name__ == "__main__":
         # Ignore hidden dirs
         dirs[:] = [d for d in dirs if not d[0] == '.']
 
-        _get_dir_checksums(root)
+        if args.action == 'generate':
+            _generate(root)
+        elif args.action == 'verify':
+            _verify(root)
